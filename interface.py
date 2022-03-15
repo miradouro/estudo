77a48877d4f351f0af92ad3ca2038f3f5a71c5ec
Rafael <rafaelmiradouro@gmail.com>
2022-03-15 19:02:20 -0300

    atualizando2

--- interface.py
+++ interface.py
@@ -3,8 +3,44 @@ from tkinter import *
 
 app = Tk()
 app.title("Eh noix!!!")
-app.geometry("500x300")
+
+largura = 600
+altura = 400
+largura_tela = app.winfo_screenwidth()
+altura_tela = app.winfo_screenheight()
+print(largura_tela, altura_tela)
+posx = largura_tela / 2 - largura / 2
+posy = altura_tela / 2 - altura / 2
+app.geometry("%dx%d+%d+%d" % (largura,altura, posx, posy))
+
+#app.geometry("600x300+400+200")
+app.resizable(0, 0)
+
+#app.maxsize(600, 600)
+#app.minsize(200, 200)
+#app.state("zoomed")
+#app.state("iconic")
+
+app.iconbitmap("images/icon.ico")
+
 app.configure(background="#008")
+
 txt1 = Label(app, text="Curso interface", background="yellow", foreground="#000")
 txt1.place(x=10, y=10, width=300, height=30)
+
+
+def cmd():
+    print("Ola mundo!")
+
+
+def cmd2(msg):
+    print(msg)
+
+
+btn = Button(app, text="executar", command=cmd)
+btn.place(x=490, y=260, width=100, height=30)
+btn2 = Button(app, text="botao 2", command=lambda: cmd2("mensagem"))
+btn2.place(x=490, y=220, width=100, height=30)
+
+
 app.mainloop()
